245b41fc176df45f17b2c0a4629c5b84daaa757b
DHIRAAJ <111226890+PROFESSOR-DJ@users.noreply.github.com>
2024-01-09 11:28:44 +0530

    Add files via upload

---
+++ OnlineTest.java
@@ -0,0 +1,155 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+class OnlineTest extends JFrame implements ActionListener
+{
+	JLabel l;
+	JRadioButton jb[]=new JRadioButton[5];
+	JButton NEXT,BOOKMARK;
+	ButtonGroup bg;
+	int count=0,current=0,x=1,y=1,now=0;
+	int m[]=new int[10];	
+	OnlineTest(String s)
+	{
+		super(s);
+		l=new JLabel();
+		add(l);
+		bg=new ButtonGroup();
+		for(int i=0;i<5;i++)
+		{
+			jb[i]=new JRadioButton();	
+			add(jb[i]);
+			bg.add(jb[i]);
+		}
+		NEXT=new JButton("Next");
+		BOOKMARK=new JButton("Bookmark");
+		NEXT.addActionListener(this);
+		BOOKMARK.addActionListener(this);
+		add(NEXT);
+		add(BOOKMARK);
+		set();
+		l.setBounds(30,40,450,20);
+		jb[0].setBounds(50,80,100,20);
+		jb[1].setBounds(50,110,100,20);
+		jb[2].setBounds(50,140,100,20);
+		jb[3].setBounds(50,170,100,20);
+		NEXT.setBounds(100,240,100,30);
+		BOOKMARK.setBounds(270,240,100,30);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLayout(null);
+		setLocation(250,100);
+		setVisible(true);
+		setSize(600,600);
+	}
+	public void actionPerformed(ActionEvent e)
+	{
+		if(e.getSource()==NEXT)
+		{
+			if(check())
+				count=count+1;
+			current++;
+			set();	
+			if(current==9)
+			{
+				NEXT.setEnabled(false);
+				BOOKMARK.setText("Result");
+			}
+		}
+		if(e.getActionCommand().equals("Bookmark"))
+		{
+			JButton bk=new JButton("Bookmark"+x);
+			bk.setBounds(480,20+30*x,100,30);
+			add(bk);
+			bk.addActionListener(this);
+			m[x]=current;
+			x++;
+			current++;
+			set();	
+			if(current==9)
+				BOOKMARK.setText("Result");
+			setVisible(false);
+			setVisible(true);
+		}
+		for(int i=0,y=1;i<x;i++,y++)
+		{
+		if(e.getActionCommand().equals("Bookmark"+y))
+		{
+			if(check())
+				count=count+1;
+			now=current;
+			current=m[y];
+			set();
+			((JButton)e.getSource()).setEnabled(false);
+			current=now;
+		}
+		}
+	
+		if(e.getActionCommand().equals("Result"))
+		{
+			if(check())
+				count=count+1;
+			current++;
+			
+			JOptionPane.showMessageDialog(this,"correct ans="+count);
+			System.exit(0);
+		}
+	}
+	void set()
+	{
+		jb[4].setSelected(true);
+		if(current==0)
+		{
+			l.setText("");
+			jb[0].setText("");jb[1].setText("");jb[2].setText("");jb[3].setText("");	
+		}
+		if(current==1)
+		{
+			l.setText("");
+			jb[0].setText("");jb[1].setText("");jb[2].setText("");jb[3].setText("");
+		}
+		if(current==2)
+		{
+			l.setText("");
+			jb[0].setText("");jb[1].setText("");jb[2].setText("");jb[3].setText("");
+		}
+		if(current==3)
+		{
+			l.setText("");
+			jb[0].setText("");jb[1].setText("");jb[2].setText("");jb[3].setText("");
+		}
+		l.setBounds(30,40,450,20);
+		for(int i=0,j=0;i<=90;i+=30,j++)
+			jb[j].setBounds(50,80+i,200,20);
+	}
+	boolean check()
+	{
+		if(current==0)
+			return(jb[1].isSelected());
+		if(current==1)
+			return(jb[2].isSelected());
+		if(current==2)
+			return(jb[3].isSelected());
+		if(current==3)
+			return(jb[0].isSelected());
+		if(current==4)
+			return(jb[2].isSelected());
+		if(current==5)
+			return(jb[2].isSelected());
+		if(current==6)
+			return(jb[1].isSelected());
+		if(current==7)
+			return(jb[3].isSelected());
+		if(current==8)
+			return(jb[1].isSelected());
+		if(current==9)
+			return(jb[2].isSelected());
+		return false;
+	}
+	public static void main(String s[])
+	{
+		new OnlineTest("Online Test Of Java");
+	}
+
+
+}
